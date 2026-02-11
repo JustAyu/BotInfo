3311953a21d1afc19c7cf3b03d25df24e9e6553f
/ 𝘈𝘠𝘜𝘚𝘏 <107471238+JustAyu@users.noreply.github.com>
2026-02-11 15:19:47 +0530

    Update start.py

--- start.py
+++ start.py
@@ -126,9 +126,7 @@ async def start_web():
 
 async def main():
     await start_web()
-    await app.start()
-    await idle()
-    await app.stop()
 
 if __name__ == "__main__":
     asyncio.run(main())
+    app.run()
